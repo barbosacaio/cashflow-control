517b25956df75ee369b757dd4dd313b44edec6c1
Seu Nome <mariaeduardaferraz343@gmail.com>
2026-05-28 18:33:36 -0300

    organiza estrutura inicial do servidor

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,15 +2,12 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 3333;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/health", (request, response) => {
-  return response.json({ status: "ok >:D" });
+  return response.json({ status: "ok" });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port} >:D`);
-});
+export { app };
