4eead13b6ffd87b317db2496258e60e9d5a6e4ab
barboscaio <caiiohenrique2004@gmail.com>
2026-06-04 00:22:10 -0300

    feat: auth routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { errorMiddleware } from './middlewares/error.middleware.js';
+import { authRoutes } from './routes/auth.routes.js';
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get('/health', (request, response) => {
   return response.json({ status: 'ok' });
 });
+
+app.use('/auth', authRoutes);
 
 app.use(errorMiddleware);
 
